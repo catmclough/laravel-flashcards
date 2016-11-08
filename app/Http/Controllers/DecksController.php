4cd9f5b11a8b59422db3aa2b7f9004e6094fa308
Catherine McLoughlin <cmcloughlin@8thlight.com>
2016-11-07 19:57:56 -0800

    Implementation that passes existing tests

--- app/Http/Controllers/DecksController.php
+++ app/Http/Controllers/DecksController.php
@@ -15,11 +15,4 @@ class DecksController extends Controller
     public function show(Deck $deck) {
         return view('decks.show', ['deck' => $deck ]);
     }
-
-    public function store(Request $request) {
-        $deck = new Deck();
-        $deck->title = $request->title;
-        $deck->save();
-        return redirect('/decks');
-    }
 }
